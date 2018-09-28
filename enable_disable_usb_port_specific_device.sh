3ec4ee072003c4903cf9d18ca0efc78286c505b1
Miguel Ortiz <miguelleonardo.ortiz@bbva.com>
2018-09-28 14:55:36 -0300

    Mejoras en script para habilitar/deshabilitar puertos usb en linux

--- enable_disable_usb_port_specific_device.sh
+++ enable_disable_usb_port_specific_device.sh
@@ -1,62 +1,93 @@
-
 #!/bin/bash
 # miguel.ortiz
-# enable-disable specific port if a device from devices.list were connected 
-
-# Usage: 
-# ./enable_disable_usb_port_specific_device.sh ON 	# Enables the device
-# ./enable_disable_usb_port_specific_device.sh OFF 	# Disable the device
-# devices.list # Must contain the ID from lsusb of the device we want to manage
-# for several devices consider adding a new variable!
-
-
-
+# enable-disable specific usb port for first device in "devices.list"
+# read: http://migueleonardortiz.com.ar/linux/learning-how-to-disable-specific-usb-devices-by-their-ports-in-linux/1645
+# to know how to setup "devices.list"
 # ---------------------------------------enable this commented lines for logging (and at the end of the script)
 #touch cheap_pinpad.log
 #exec 1> >(tee -a cheap_pinpad.log) 2>&1
 
-readarray DEVICES < devices.list
-LOCK=pinpad.lock
+LOCK='/tmp/pinpad.lock'
 BINDS='/sys/bus/usb/drivers/usb'
 BINDF="$BINDS/bind"
 UNBINDF="$BINDS/unbind"
+STATUS=$1
+# ----VERIFY PARAMETERS ----
+
+
+if [ "$STATUS" == 'ON' ]
+then 
+	:	
+elif [ "$STATUS" == 'OFF' ]
+then
+	:
+else
+	exit 3 # Invalid parameter for this script
+fi
+
 # -----DEFINE PORT ------
-if [ ! -f $LOCK ]; then
+readarray DEVICES < /usr/local/bin/devices.list
+
+if [ ! -f $LOCK ]
+then
         for device in "${DEVICES[@]}" ; do
-                PORTID=$(grep -l $device /sys/bus/usb/devices/*/uevent | tail -1)
-                if [ "$PORTID" == '' ]; then
+                DEVPORT=`lsusb | grep $device |awk '{print $2"/"$4}' |tr -d ':'`
+		PORTIDX=$(grep -l $DEVPORT /sys/bus/usb/devices/*/uevent | tail -1)
+                if [ "$PORTIDX" == '' ]
+		then
+			exit 4 # no encuentro el dispositivo
                         :
                 else
-                        PORTID=$(echo $PORTID | tr "/" " "|awk '{print $5}')
+			PORTID=$(echo $PORTIDX | tr "/" " "|awk '{print $5}')
+			`echo $PORTID > $LOCK`
                 fi
         done
 fi
 
+# ------ DEVICE EXISTENCE -------
+
+function verify_device() {
+	DEVEXISTS=$(grep -l ".*" /sys/bus/usb/devices/*/uevent | grep $1)
+	if [ "$DEVEXISTS" == '' ]
+	then
+		exit 5 # no encuentro el dispositivo, al parecer fue desconectado 
+	else 
+		:	
+	fi
+}
+
+
 # ----- INTERACT -------
-    
-if [ -s "$LOCK" ]; then
-        DEVPORT=$(cat pinpad.lock)        
-else
-        echo 'ERROR: Device not registered correctly'
-        exit 1
-fi  
+   
+if [ -s "$LOCK" ]
+then
+	DEVPORT=$(cat pinpad.lock)     
+	verify_device $DEVPORT
+else 
+        exit 6 # se registro mal el dispositivo en pinpad.lock
+fi 
 
-if [ "$1" == 'ON' ] && [ ! -e "$BINDS/$DEVPORT"  ] ; then 
-        $(echo -n $DEVPORT > $BINDF)
-        OK='PORT Enabled'
-else :
-        ERROR='ERROR, port already enabled'
+# ---- Encendido
+if [ "$STATUS" == "ON" ] && [ ! -e "$BINDS/$DEVPORT" ]
+then
+	$(echo -n $DEVPORT > $BINDF) 
+
+elif [ "$STATUS" == "ON" ] && [ -e "$BINDS/$DEVPORT" ]
+then
+	exit 7 # El dispositivo ya ha sido habilitado
 fi
 
+# ---- Apagado
+if [ "$STATUS" == 'OFF' ] && [ -e "$BINDS/$DEVPORT" ]
+then
+	$(echo -n $DEVPORT > $UNBINDF )
 
-if [ "$1" == 'OFF' ] && [ -e "$BINDS/$DEVPORT" ]; then
-        $(echo -n $DEVPORT > $UNBINDF)
-        OK='PORT Disabled'
-else :
-        ERROR='ERROR, port already disabled'
+elif [ "$STATUS" == 'OFF' ] && [ ! -e "$BINDS/$DEVPORT" ]
+then
+	exit 8 # Ya esta deshabilitado el puerto USB
 fi
 
-# ---------------------------------------enable this commented lines for logging 
+# ---------------------------------------enable this commented lines for logging
 #echo $ERROR >> cheap_pinpad.log
 #echo $OK >> cheap_pinpad.log
-                                                                                                                                    29,1        Final
+
