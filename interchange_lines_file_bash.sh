77fb5812082bf03e533c08b1b5b51e9deae9225b
miguelleonardo.ortiz@bbva.com <miguelleonardo.ortiz@bbva.com>
2019-01-24 17:51:06 -0300

    added details

--- interchange_lines_file_bash.sh
+++ interchange_lines_file_bash.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # miguel.ortiz
 # as an answer for https://stackoverflow.com/questions/54353965/changing-two-lines-of-a-text-file/54354995#54354995
+# Create a file, then select two lines to be interchanged and pass them as parameters
+# Example: interchange_lines_file_bash.sh 3 7 (this would interchange line 7 for 3 and 3 for 7 in the output file). 
 
 ORIGFILE='original.txt'  # original text file
 PROCFILE='processed.txt' # copy of the original file to be proccesed
