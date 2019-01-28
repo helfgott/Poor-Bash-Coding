a1b2762c4625cf9f1e2b7c0573fbede8c62851c5
miguelleonardo.ortiz@bbva.com <miguelleonardo.ortiz@bbva.com>
2019-01-28 15:21:16 -0300

    Fix, expansion doesn't work with if as plain in previous ver.

--- if_multiple_files_change_owner/if_multiple_files_change_owner.sh
+++ if_multiple_files_change_owner/if_multiple_files_change_owner.sh
@@ -1,21 +1,18 @@
 #! /bin/bash
 # miguel.ortiz
-# This scripts evaluate if specific files: file_a, file_b and file_c exists in the directory
+# This scripts evaluate if specific files: file_a, file_b and file_c exists in the directory and change
+# owner/group to root
 # For all files in a directory use metacharacter "*" instead brace expansion.
 
 # directory to check
-DIR_TO_CHECK='/home/mortiz/Documents/projects/Poor-Bash-Coding/if_multiple_files_change_owner/'
+DIR_TO_CHECK='/home/mortiz/Documents/projects/Poor-Bash-Coding/if_multiple_files_change_owner'
 
 # array of files, using brace expansion
-files_to_check=($DIR_TO_CHECK{file_a,file_b,file_c})
+files_to_check=($DIR_TO_CHECK/{file_a,file_b,file_c})
 
 # -f (file exists)
 # ${files[@]}  # Elements on the array (the full path of each file)
 # ${#files[@]} # Use this if you want to access the number of files in the path
 
-if [ -f ${files[@]} ] 
-then
-	echo 'Files a,b,c exists' 
-else 
-	echo 'One or more files do not exists'
-fi
+for i in ${files_to_check[@]}; do if [ -f $i ]; then chown root: "$i"; fi; done
+
